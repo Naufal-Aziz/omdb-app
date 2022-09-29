67660209d77ba4fbe6b72bd473a719d497c2b1e3
aziz muhammad naufal <ajosnaufal@gmail.com>
2022-09-29 12:58:34 +0700

    JSM React crash course, follow along create a movie database app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-function App() {
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=1aa1c0a0";
+
+
+
+const App = () => {
+  const theme = localStorage.getItem('theme')
+
+  let [stateTheme, setStateTheme] = useState(theme)
+
+  let [movieList, setMovieList] = useState([]);
+
+  let [page, setPage] = useState(1);
+
+  let [keyword, setKeyword] = useState("");
+
+  const themeSwitch = () => {
+    console.log(stateTheme)
+    if (!stateTheme || stateTheme == "light") {
+      setStateTheme('dark')
+      localStorage.setItem("theme", "dark");
+    } else {
+      setStateTheme('light')
+      localStorage.setItem("theme", "light");
+    }
+  };
+
+  const searchMovies = async (title) => {
+    const response = await axios.get(`${API_URL}&s=${title}&page=${page}`);
+
+    setMovieList(response.data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies(keyword);
+  }, [page]);
+
+  useEffect(() => {}, [stateTheme]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={
+        stateTheme == "dark" ? { backgroundColor: "black", color: "white" } : {}
+      }
+    >
+      <button onClick={themeSwitch}>
+        {stateTheme == "dark" ? "GO LIGHT" : "GO DARK"}
+      </button>
+      <h1>Hello, {keyword}!</h1>
+      <input
+        type="text"
+        value={keyword}
+        onChange={(e) => {
+          setKeyword(e.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          searchMovies(keyword);
+        }}
+      >
+        SEARCH
+      </button>
+      <ul className="movie-list">
+        {movieList && movieList.length > 0 ? (
+          <>
+            {movieList.map((item, index) => (
+              <li key={index}>
+                <h3>{item.Title}</h3>
+                <img
+                  src={
+                    item.Poster !== "N/A"
+                      ? item.Poster
+                      : "https://dummyimage.com/300x400/000/ffffff&text=No+Poster"
+                  }
+                  alt=""
+                />
+                <div className="movie-detail">
+                  <p>Type : {item.Type}</p>
+                  <p>Year : {item.Year}</p>
+                </div>
+              </li>
+            ))}
+          </>
+        ) : (
+          <li> ajos </li>
+        )}
+      </ul>
+      <button
+        onClick={() => {
+          setPage(page - 1);
+        }}
+      >
+        Prev page
+      </button>
+      <button
+        onClick={() => {
+          setPage(page + 1);
+        }}
+      >
+        Next Page
+      </button>
     </div>
   );
-}
+};
 
 export default App;
